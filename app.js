5bae500b0b550f213bff0338d499b7f1c2f5f0e0
Son, David <David.Son@ca.com>
2017-08-20 21:35:39 -0500

    Change winner score 100 to 50

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
 		//Update the UI
 		document.querySelector('#score-'+activePlayer).textContent = scores[activePlayer];
 		//Check if player won the game
-		if (scores[activePlayer]>=100){
+		if (scores[activePlayer]>=50){
 			document.querySelector('#name-'+activePlayer).textContent='Winner!';
 			document.querySelector('.dice').style.display='none';
 			document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
